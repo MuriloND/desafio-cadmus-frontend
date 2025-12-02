4d6ab2b1d515bc4370763d801aae07a86811bc49
Murilo Nogueira Duarte <murilondev@gmail.com>
2025-12-01 22:54:32 -0300

    feat(app/* & features/auth/*): Added login page, auth token generation and storage

---
+++ features/auth/actions.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface LoginState {
+  error?: string;
+}
+
+export async function loginAction(prevState: LoginState, formData: FormData) {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/sign-in`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return { error: data.error || "Erro ao realizar login" };
+    }
+
+    const cookieStore = await cookies();
+    
+    cookieStore.set("cadmus.token", data.token, {
+      httpOnly: false, 
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7, // 7 dias
+      path: "/", 
+    });
+
+  } catch (err) {
+    console.error(err);
+    return { error: "Falha na comunicação com o servidor." };
+  }
+
+  console.log("LOGOU")
+  redirect("/dashboard");
+}
